7ce17bd767fa172bfd758ceb4dfc4f6c3abc07aa
Niraj Rawat <niraj@Nirajs-MacBook-Air.local>
2026-01-11 05:30:01 +0000

    created the components and files

---
+++ src/components/CheckList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckList = () => {
+  return <div>CheckList</div>;
+};
+
+export default CheckList;
---
+++ src/components/RegisterMain.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterMain = () => {
+  return <div>RegisterMain</div>;
+};
+
+export default RegisterMain;
